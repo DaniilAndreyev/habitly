902f8345d987574235587ac5bdd8b6275fcb2a4f
SlavaPletnov <cslava868@gmail.com>
2025-11-29 11:42:46 -0500

    Wrap app inside of HabitContext

--- habitly/src/main.jsx
+++ habitly/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import { HabitProvider } from './contexts/HabitContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <HabitProvider>
+        <App />
+      </HabitProvider>
     </AuthProvider>
   </StrictMode>,
 )
